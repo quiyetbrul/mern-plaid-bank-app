a3e3add59d351db8c3d76018000215664630d151
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-01-12 17:48:40 -0800

    middleare and compose removed for mobile app

--- client/src/store.js
+++ client/src/store.js
@@ -3,12 +3,8 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const initialState = {};
-const middleware = [thunk];
 const store = createStore(
     rootReducer,
     initialState,
-    compose(
-        applyMiddleware(...middleware),
-    )
 );
 export default store;
