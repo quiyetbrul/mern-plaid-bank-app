68ebdcfb42b71e539fd92364fa4b99b1117dfce0
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-01-12 18:19:44 -0800

    mobile should load properly

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Live demo:
 https://serene-wave-06051.herokuapp.com/
 
 Active email is required.
+
+When linking a "bank" to your account, use this fake credentials:
+
+username: user_good
+password: pass_good
--- client/src/store.js
+++ client/src/store.js
@@ -3,8 +3,10 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const initialState = {};
+const middleware = [thunk];
 const store = createStore(
     rootReducer,
     initialState,
+    applyMiddleware(...middleware)
 );
 export default store;
